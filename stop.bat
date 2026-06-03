646d2d0227eaa2357b3e86fcd1912ce47a3d6a3b
M-Usman10 <muhammadusman1.me@gmail.com>
2026-06-03 20:48:02 +0500

    Simplify stop.bat: just kill all pythonw.exe processes
    Process filtering was unreliable cross-platform. Since pythonw.exe
    is only used by this bot, killing all instances is safe.

--- stop.bat
+++ stop.bat
@@ -1,7 +1,5 @@
 @echo off
 echo Stopping Discord Screenshot Bot...
-
-powershell -Command "Get-CimInstance Win32_Process | Where-Object { $_.Name -eq 'pythonw.exe' -and $_.CommandLine -like '*main.py*' } | ForEach-Object { Write-Host 'Killing PID' $_.ProcessId; Stop-Process -Id $_.ProcessId -Force }"
-
+taskkill /f /im pythonw.exe
 echo Done.
 pause
