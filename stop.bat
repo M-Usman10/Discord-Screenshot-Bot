1a681abe7524c7b298f8f02cdf3eba46b114d948
M-Usman10 <muhammadusman1.me@gmail.com>
2026-06-03 20:41:34 +0500

    Fix stop.bat for Win11: replace Get-WmiObject with Get-CimInstance
    Get-WmiObject is deprecated and unreliable on Windows 11.

--- stop.bat
+++ stop.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Stopping Discord Screenshot Bot...
 
-powershell -Command "Get-WmiObject Win32_Process | Where-Object { $_.Name -eq 'pythonw.exe' -and $_.CommandLine -like '*main.py*' } | ForEach-Object { Write-Host 'Killing PID' $_.ProcessId; Stop-Process -Id $_.ProcessId -Force }"
+powershell -Command "Get-CimInstance Win32_Process | Where-Object { $_.Name -eq 'pythonw.exe' -and $_.CommandLine -like '*main.py*' } | ForEach-Object { Write-Host 'Killing PID' $_.ProcessId; Stop-Process -Id $_.ProcessId -Force }"
 
 echo Done.
 pause
