2d152ba80bf0bd00c705571f2c060dc20ca16032
M-Usman10 <muhammadusman1.me@gmail.com>
2026-06-03 18:28:44 +0500

    Fix stop.bat: use PowerShell instead of wmic for reliable process kill

--- stop.bat
+++ stop.bat
@@ -1,11 +1,7 @@
 @echo off
 echo Stopping Discord Screenshot Bot...
 
-:: Find pythonw.exe processes running main.py from this folder and kill only those
-for /f "tokens=2" %%i in ('wmic process where "name='pythonw.exe' and commandline like '%%main.py%%'" get processid /format:list ^| findstr "="') do (
-    echo Killing PID %%i
-    taskkill /f /pid %%i
-)
+powershell -Command "Get-WmiObject Win32_Process | Where-Object { $_.Name -eq 'pythonw.exe' -and $_.CommandLine -like '*main.py*' } | ForEach-Object { Write-Host 'Killing PID' $_.ProcessId; Stop-Process -Id $_.ProcessId -Force }"
 
 echo Done.
 pause
